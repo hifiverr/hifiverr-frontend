8238aa725d62e7fc4016839f8c72535943c87413
Filipe Pires <flippires@gmail.com>
2021-06-28 21:53:47 +0100

    add css to signup

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import "./SignUp.css" ;
 
 const SignUp = () => {
   const [state, setState] = React.useState({});
@@ -19,7 +20,7 @@ const SignUp = () => {
 
   return (
     <div className="signup-wrapper">
-      <p>Sign Up</p>
+      <p className="h1-signup">Sign Up</p>
       <form onSubmit={handleSubmit}>
         <p>Image</p>
         <input
@@ -30,7 +31,12 @@ const SignUp = () => {
         />
         <br />
         <p>Email</p>
-        <input name="email" value={state.email} onChange={handleChange} />
+        <input
+          name="email"
+          value={state.email}
+          onChange={handleChange}
+          className="signup-email-input"
+        />
         <br />
         <p>Password</p>
         <input
@@ -38,6 +44,7 @@ const SignUp = () => {
           name="password"
           value={state.password}
           onChange={handleChange}
+          className="password-input"
         />
         <br />
         <p>First Name</p>
@@ -45,36 +52,61 @@ const SignUp = () => {
           name="firstname"
           value={state.firstname}
           onChange={handleChange}
+          className="firstname-input"
         />
         <br />
         <p>Last Name</p>
-        <input name="lastname" value={state.lastname} onChange={handleChange} />
+        <input
+          name="lastname"
+          value={state.lastname}
+          onChange={handleChange}
+          className="lastname-input"
+        />
         <br />
         <p>Location</p>
-        <input name="location" value={state.location} onChange={handleChange} />
+        <input
+          name="location"
+          value={state.location}
+          onChange={handleChange}
+          className="location-input"
+        />
         <br />
         <p>Language</p>
         <input
           name="language"
           value={state.primary_language}
           onChange={handleChange}
+          className="language-input"
         />
         <br />
         <p>Skills</p>
-        <input name="skills" value={state.skills} onChange={handleChange} />
+        <input
+          name="skills"
+          value={state.skills}
+          onChange={handleChange}
+          className="skill-input"
+        />
         <br />
         <p>Interests</p>
         <input
           name="interests"
           value={state.Interests}
           onChange={handleChange}
+          className="interests-input"
         />
         <br />
         <p>About Me</p>
-        <input name="aboutme" value={state.aboutme} onChange={handleChange} />
+        <input
+          name="aboutme"
+          value={state.aboutme}
+          onChange={handleChange}
+          className="aboutme-input"
+        />
         <br />
 
-        <button type="submit">Sign Up</button>
+        <button type="submit" className="signup-button">
+          Sign Up
+        </button>
       </form>
     </div>
   );
