7b571d13263c978ac8aac0bfb267ea3a3cb32081
Filipe Pires <flippires@gmail.com>
2021-07-05 09:29:11 +0100

    finishing some errors

--- src/components/Community/CommunityDetails.js
+++ src/components/Community/CommunityDetails.js
@@ -75,102 +75,103 @@ const CommunityDetails = ({ community, setCommunity, match }) => {
   console.log(community);
 
   return (
-    <div className="community-container">
+    <>
       <Header />
       <div className="community-details-name">
         <h1>{communityInfo.name}</h1>
       </div>
-
-      <h1 className="h1-community-post event-first-title">Events</h1>
-      <div className="community-details-events-flex-container">
-        {eventsByCommunity.map((event) => {
-          return (
-            <div className="community-details-event-flex-item">
-              <div className="events-img-container">
-                <img
-                  className="community-details-cover"
-                  src={event.event_image}
-                  alt=""
-                />
+      <div className="community-container">
+        <h1 className="h1-community-post event-first-title">Events</h1>
+        <div className="community-details-events-flex-container">
+          {eventsByCommunity.map((event) => {
+            return (
+              <div className="community-details-event-flex-item">
+                <div className="events-img-container">
+                  <img
+                    className="community-details-cover"
+                    src={event.event_image}
+                    alt=""
+                  />
+                </div>
+                <p className="h1-community-events">
+                  {event.firstname} {event.lastname}
+                </p>
+                <p className="h1-community-events">{event.event_title}</p>
+                <p className="h1-community-events">{event.event_date}</p>
+                <a
+                  href={event.link}
+                  target="_blank"
+                  className="h1-community-events"
+                >
+                  Join
+                </a>
               </div>
-              <p className="h1-community-events">
-                {event.firstname} {event.lastname}
-              </p>
-              <p className="h1-community-events">{event.event_title}</p>
-              <p className="h1-community-events">{event.event_date}</p>
-              <a
-                href={event.link}
-                target="_blank"
-                className="h1-community-events"
-              >
-                Join
-              </a>
+            );
+          })}
+        </div>
+
+        <Link
+          to={{
+            pathname: "/add-event",
+            state: { community_id: communityId },
+          }}
+          className="add-event-button"
+        >
+          <span className="add-event-plus-sign">+</span> Add event
+        </Link>
+        <div>
+          <h1 className="h1-community-post">Feed</h1>
+
+          <form onSubmit={createPost}>
+            <textarea
+              id="post_content"
+              name="post_content"
+              onChange={handleChange}
+              value={newPost.post_content}
+              placeholder="Share your ideas..."
+              className="text-area"
+              style={{
+                width: "94%",
+                height: "30px",
+                padding: "10px",
+                background: "#e5e5e5",
+                border: "none",
+              }}
+            ></textarea>
+            <button type="submit" className="comment-button">
+              Share
+            </button>
+          </form>
+        </div>
+
+        <div
+          style={{
+            height: "40px",
+            marginBottom: "40px",
+          }}
+        />
+
+        {postsByComunity.map((post) => {
+          return (
+            <div style={{ marginTop: "50px" }}>
+              <div className="community-post-container">
+                <Link to={`/profile/${post.user_id}`} className="profile-image">
+                  <img
+                    className="community-post-image"
+                    src={post.profile_image}
+                    alt=""
+                  />
+                </Link>
+                <div style={{ position: "relative", width: "100%" }}>
+                  <p className="post-content">{post.post_content}</p>
+                  <button className="comment-button">Comment</button>
+                </div>
+              </div>
             </div>
           );
         })}
       </div>
-
-      <Link
-        to={{
-          pathname: "/add-event",
-          state: { community_id: communityId },
-        }}
-        className="add-event-button"
-      >
-        + Add event
-      </Link>
-      <div>
-        <h1 className="h1-community-post">Posts</h1>
-
-        <form onSubmit={createPost}>
-          <textarea
-            id="post_content"
-            name="post_content"
-            onChange={handleChange}
-            value={newPost.post_content}
-            placeholder="Share your ideas..."
-            className="text-area"
-            style={{
-              width: "94%",
-              height: "30px",
-              padding: "10px",
-              background: "#e5e5e5",
-              border: "none",
-            }}
-          ></textarea>
-          <button type="submit" className="comment-button">
-            Share
-          </button>
-        </form>
-      </div>
-
-      <div
-        style={{
-          height: "40px",
-          marginBottom: "40px",
-        }}
-      />
-
-      {postsByComunity.map((post) => {
-        return (
-          <div style={{ marginTop: "50px" }}>
-            <div className="community-post-container">
-              <Link to={`/profile/${post.user_id}`} className="profile-image">
-                <img
-                  className="community-post-image"
-                  src={post.profile_image}
-                  alt=""
-                />
-              </Link>
-              <div style={{ position: "relative", width: "100%" }}>
-                <p className="post-content">{post.post_content}</p>
-                <button className="comment-button">Comment</button>
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    </>
   );
 };
 
